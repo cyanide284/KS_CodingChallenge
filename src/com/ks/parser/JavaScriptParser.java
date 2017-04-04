b58bd9d6734b2c62cf4171cf2fe0f06ad0416d70
cyanide284 <abhinav284@gmail.com>
2017-04-04 02:00:26 -0400

    feat(jsparser): parse the js file
    parse the js file and store it in a HashMap with line number as key and code as value

--- src/com/ks/parser/JavaScriptParser.java
+++ src/com/ks/parser/JavaScriptParser.java
@@ -1,5 +1,56 @@
 package com.ks.parser;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class JavaScriptParser {
+	
+	public HashMap<Integer,String> readFile(String FILENAME) {
+
+		BufferedReader br = null;
+		FileReader fr = null;
+		
+		HashMap<Integer, String> parsedJS = new HashMap<Integer, String>();
+
+		try {
+			
+			fr = new FileReader(FILENAME);
+			br = new BufferedReader(fr);
+
+			String currentLine;
+			int lineNumber = 0;
+			
+			while ((currentLine = br.readLine()) != null) {
+				lineNumber++;
+				parsedJS.put(lineNumber, currentLine);
+			}
+			
+		}
+
+		catch (IOException ioe) {
+			ioe.getMessage();
+		}
+
+		finally {
+
+			try {
+
+				if(br!=null)
+					br.close();
+				if(fr!=null)
+					fr.close();
+
+			}
+			
+			catch (IOException ioe) {
+				ioe.getMessage();
+			}
+		}
+		
+		return parsedJS;
+
+	}
 	
 }
