8d036d58781773c4a3f186adbce0cf93a2372694
cyanide284 <abhinav284@gmail.com>
2017-04-04 03:04:20 -0400

    feat(findSingleLinedConditionals): implement the method
    implement method to find if/else without a block

--- src/com/ks/jslint/JavaScriptLintImpl.java
+++ src/com/ks/jslint/JavaScriptLintImpl.java
@@ -1,29 +1,46 @@
 package com.ks.jslint;
 
-public class JavaScriptLintImpl implements JavaScriptLintInterface{
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.ks.parser.JavaScriptParser;
+
+public class JavaScriptLintImpl implements JavaScriptLintInterface {
 
 	@Override
 	public void findUnusedVariables() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findSingleLinedConditionals(String fileName) {
-		// TODO Auto-generated method stub
-		
+		HashMap<Integer, String> parsedJS = new JavaScriptParser().readFile(fileName);
+		Iterator iter = parsedJS.entrySet().iterator();
+
+		while (iter.hasNext()) {
+			Map.Entry pair = (Map.Entry) iter.next();
+			String currentLine = pair.getValue().toString();
+			if (currentLine.contains("if") || currentLine.contains("else")) {
+				if (currentLine.trim().substring(0, 2).equalsIgnoreCase("if")
+						|| currentLine.trim().substring(0, 4).equalsIgnoreCase("else")) {
+					System.out.println(pair.getKey() + "." + " " + pair.getValue());
+				}
+			}
+		}
 	}
 
 	@Override
 	public void findUndeclaredFunctions() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findUnbalancedBraces() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
