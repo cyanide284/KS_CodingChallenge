471c39dcf450f346604749050bcb37ce55e79bfb
cyanide284 <abhinav284@gmail.com>
2017-04-04 02:23:19 -0400

    feat(JavaScriptLintImpl): implement interface JavaScriptLintInterface
    implement the interface with a skeleton of overriden methods

---
+++ src/com/ks/jslint/JavaScriptLintImpl.java
@@ -0,0 +1,29 @@
+package com.ks.jslint;
+
+public class JavaScriptLintImpl implements JavaScriptLintInterface{
+
+	@Override
+	public void findUnusedVariables() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findSingleLinedConditionals(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findUndeclaredFunctions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findUnbalancedBraces() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
