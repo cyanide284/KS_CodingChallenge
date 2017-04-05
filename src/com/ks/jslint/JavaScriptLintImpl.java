8edbfafc72f8252d44984ce1c01bad5953cfd7e8
cyanide284 <abhinav284@gmail.com>
2017-04-04 22:48:50 -0400

    feat(JavaScriptLintImpl): implement findUnbalancedBraces()
    implement method to find unbalanced braces and also add comments

--- src/com/ks/jslint/JavaScriptLintImpl.java
+++ src/com/ks/jslint/JavaScriptLintImpl.java
@@ -3,6 +3,7 @@ package com.ks.jslint;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Stack;
 
 import com.ks.parser.JavaScriptParser;
 
@@ -14,9 +15,13 @@ public class JavaScriptLintImpl implements JavaScriptLintInterface {
 
 	}
 
+	// Method to find single-lined if else statements not enclosed in a code
+	// block
 	@Override
 	public void findSingleLinedConditionals(String fileName) {
 		HashMap<Integer, String> parsedJS = new JavaScriptParser().readFile(fileName);
+
+		// Using entrySet as HashMap does not have an iterator
 		Iterator iter = parsedJS.entrySet().iterator();
 
 		while (iter.hasNext()) {
@@ -37,14 +42,35 @@ public class JavaScriptLintImpl implements JavaScriptLintInterface {
 
 	}
 
+	// Method to find unbalanced or missing braces
 	@Override
 	public void findUnbalancedBraces(String fileName) {
+
+		// using a stack to store the opening braces '{'
+		Stack<Character> stack = new Stack<Character>();
+
 		HashMap<Integer, String> parsedJS = new JavaScriptParser().readFile(fileName);
 		Iterator iter = parsedJS.entrySet().iterator();
 
 		while (iter.hasNext()) {
 			Map.Entry pair = (Map.Entry) iter.next();
 			String currentLine = pair.getValue().toString();
+			for (int i = 0; i < currentLine.length(); i++) {
+				char ch = currentLine.charAt(i);
+				if (ch == '{') {
+					stack.push(ch);
+				} else if (ch == '}') {
+					if (stack.isEmpty()) {
+						System.out.println("Extra closing bracket on line " + pair.getKey());
+					} else {
+						stack.pop();
+					}
+				}
+			}
+
+		}
+		if(!stack.empty()){
+			System.out.println("Extra curly bracket");
 		}
 
 	}
