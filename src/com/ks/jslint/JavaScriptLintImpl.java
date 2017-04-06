8a6279c7c304c4a34c7596cd64a6ab54480a12b7
cyanide284 <abhinav284@gmail.com>
2017-04-05 21:44:39 -0400

    refactor(JavaScriptLintImp): correct and complete findUnusedVariables()
    add another hashmap to store tokens apart from variables, better performance

--- src/com/ks/jslint/JavaScriptLintImpl.java
+++ src/com/ks/jslint/JavaScriptLintImpl.java
@@ -12,22 +12,23 @@ public class JavaScriptLintImpl implements JavaScriptLintInterface {
 	// method to detect unused variables
 	@Override
 	public void findUnusedVariables(String fileName) {
-		
-		//data structure for storing declared variables
-		HashMap<String, Integer> variables = new HashMap<String, Integer>();
-		
+
+		// data structure for storing declared variables
+		HashMap<String, String> variables = new HashMap<String, String>();
+		// data structure for storing all other tokens apart from variables
+		HashMap<String, Integer> tokens = new HashMap<String, Integer>();
+
 		HashMap<Integer, String> parsedJS = new JavaScriptParser().readFile(fileName);
 		// Using entrySet as HashMap does not have an iterator
 		Iterator iter = parsedJS.entrySet().iterator();
-		
-		//parsing the code to detect and store declared variables 
+
+		// parsing the code to detect and store declared variables
 		while (iter.hasNext()) {
-			
+
 			Map.Entry pair = (Map.Entry) iter.next();
 			String currentLine = pair.getValue().toString();
-			
-			if (!currentLine.contains("function")
-					&& (currentLine.contains("var") || currentLine.contains("const") || currentLine.contains("let"))) {
+
+			if ((currentLine.contains("var") || currentLine.contains("const") || currentLine.contains("let"))) {
 				if (currentLine.trim().substring(0, 3).equals("var") || currentLine.trim().substring(0, 3).equals("let")
 						|| currentLine.trim().substring(0, 5).equals("const")) {
 					String substr[] = currentLine.trim().split(" ");
@@ -35,31 +36,46 @@ public class JavaScriptLintImpl implements JavaScriptLintInterface {
 						if (!s.equals("var")) {
 							if (s.contains("=")) {
 								String subsubstr[] = s.split("=");
-								variables.put(subsubstr[0], 1);
+								variables.put(subsubstr[0], pair.getKey().toString());
 								break;
 							} else if (s.contains(";")) {
 								String subsubstr[] = s.split(";");
-								variables.put(subsubstr[0], 1);
+								variables.put(subsubstr[0], pair.getKey().toString());
 								break;
 							} else {
-								variables.put(substr[1], 1);
+								variables.put(substr[1], pair.getKey().toString());
 								break;
 							}
 
 						}
-						
-					} //end of for loop 
-					
-				} //end of if to detect var,let,const
 
-			} //end of outermost if
+					} // end of for loop
 
-		}//end of iterating over entryset
+				} // end of if to detect var,let,const
+				
+			//storing all other tokens/keywords in another hashmap tokens	
+			} else {
+				String token[] = currentLine.trim().split("=|\\.| ");
+				for(String s:token){
+					if(tokens.containsKey(s)){
+						tokens.replace(s,tokens.get(s)+1);
+					}
+					else{
+						tokens.put(s, 1);
+					}
+				}
+			}
 
+		} // end of iterating over entryset
+		
+		//comparing both hashmaps and checking for unused variables
 		Iterator iter2 = variables.entrySet().iterator();
 		while (iter2.hasNext()) {
 			Map.Entry pair = (Map.Entry) iter2.next();
-			System.out.println(pair.getKey() + " " + pair.getValue());
+			//System.out.println(pair.getKey() + " " + pair.getValue());
+			if(!tokens.containsKey(pair.getKey())){
+				System.out.println("Unused variable on line " + pair.getValue());
+			}
 
 		}
 	}
