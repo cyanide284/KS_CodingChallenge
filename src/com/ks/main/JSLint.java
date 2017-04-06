c2d37ffbedf7b856afad3f2927855c5c644ca2d0
cyanide284 <abhinav284@gmail.com>
2017-04-06 16:07:49 -0400

    feat(JSLint): class with main method
    add JSLint to the repository. contains main method.

--- src/com/ks/main/JSLint.java
+++ src/com/ks/main/JSLint.java
@@ -1,16 +1,20 @@
 package com.ks.main;
 
+import java.util.HashMap;
+
 import com.ks.jslint.JavaScriptLintImpl;
+import com.ks.parser.JavaScriptParser;
 
 public class JSLint {
 	
 	public static void main(String[] args) {
 		String fileName = args[0];
+		HashMap<Integer, String> parsedJS = new JavaScriptParser().readFile(fileName);
 		JavaScriptLintImpl jsLint = new JavaScriptLintImpl();
-		jsLint.findSingleLinedConditionals(fileName);
-		jsLint.findUnbalancedBraces(fileName);
-		jsLint.findUnusedVariables(fileName);
-		jsLint.findUndeclaredFunctions(fileName);
+		jsLint.findUnusedVariables(parsedJS);
+		jsLint.findSingleLinedConditionals(parsedJS);
+		jsLint.findUnbalancedBraces(parsedJS);
+		jsLint.findUndeclaredFunctions(parsedJS);
 	}
 
 }
